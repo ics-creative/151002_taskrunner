55c09b8d67207cd609a9ef3751df871518d9992b
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-07 14:14:38 +0900

    grunt - ファイルの監視

---
+++ 03_watch_files_grunt/gruntfile.js
@@ -0,0 +1,21 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    watch: {
+      scripts: {
+        // 監視するファイルのパス
+        files: ["js/**/*.js"],
+        // ファイルに変更があった場合に実行されるタスク
+        tasks: ["filechanged"]
+      },
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  
+  grunt.registerTask("filechanged", "default task", function (){
+      console.log("file changed");
+  });
+  grunt.registerTask("default", "default task", function (){
+      grunt.task.run("watch");
+  });
+};
