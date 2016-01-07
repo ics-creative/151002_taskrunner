7496fa7a15b68ea996ece0755dafb26ab5b64d5b
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-07 14:25:29 +0900

    gulp - ファイルの監視

---
+++ 03_watch_files_gulp/gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require("gulp");
+
+gulp.task("watch", function(){
+  // 監視するファイルのパス
+  gulp.watch("js/**/*.js", function(event) {
+     // ファイルの変更後実行したい処理
+     console.log("file changed");
+  });
+});
+
+gulp.task("default", ["watch"]);
