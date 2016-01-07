e618f780cfe223056d6d6a404603971b1be3c2fe
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-07 13:24:15 +0900

    grunt - ファイルのコピー

---
+++ 01_copy_files_grunt/gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    copy: {
+      main: {
+        // コピー元ディレクトリの指定
+        src: "src/*",
+        // コピー先ディレクトリの指定
+        dest: "dist/",
+      },
+    }
+  });
+  grunt.loadNpmTasks("grunt-contrib-copy");
+    
+  grunt.registerTask("default", "default task", function (){
+    grunt.task.run("copy");
+  });
+};
---
+++ 01_copy_files_grunt/src/goodby.js
@@ -0,0 +1,1 @@
+i'm goodby.js
