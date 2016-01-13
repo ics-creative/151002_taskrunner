9a8d39c0013270ae4ba5a92552d4d74f64f46a2f
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-13 18:59:00 +0900

    grunt - Sassのコンパイル

---
+++ 07_compile_sass_grunt/gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files:  [{
+          expand: true,
+          // scssファイルの配置ディレクトリ
+          cwd: "sass",
+          // scssファイルのマッチパターンの指定
+          src: ["**/*.scss"],
+          // ディレクトリの指定
+          dest: "css",
+          // 拡張子の指定
+          ext: ".css"
+        }]
+      }
+    }
+  });
+    
+  grunt.loadNpmTasks("grunt-contrib-sass");
+  grunt.registerTask("default", ["sass"]);
+}
