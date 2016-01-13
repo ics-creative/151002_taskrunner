69ef580b8a532223dec66d725263b29425efa90c
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-13 18:54:35 +0900

    gulp - TypeScriptのコンパイル

---
+++ 06_compile_typescript_gulp/ts/Greeter.ts
@@ -0,0 +1,10 @@
+var Greeter = (function () {
+    function Greeter(greeting) {
+        this.greeting = greeting;
+    }
+    Greeter.prototype.greet = function () {
+        return "<h1>" + this.greeting + "</h1>";
+    };
+    return Greeter;
+})();
+;
