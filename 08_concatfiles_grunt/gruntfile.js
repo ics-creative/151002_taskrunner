b046303641a79647288a8c913ebbee04b457fd6f
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-13 21:16:33 +0900

    grunt - ファイルの結合

---
+++ 08_concatfiles_grunt/gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    concat: {
+      options: {
+        separator: ";",
+      },
+      dist: {
+        src: [
+          "css/abc.css",
+          "css/def.css",
+          "css/ghi.css"],
+        dest: "dist/css/style.css",
+      },
+    },
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.registerTask("default",["concat"]);
+}
