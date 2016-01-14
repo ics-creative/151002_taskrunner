c503014207324bced9867e9ebcbc7f05c428b79b
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-14 16:15:17 +0900

    grunt - ファイルの移動

---
+++ 09_movefile_grunt/gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    rename: {
+      main: {
+        files: [
+      		{
+      		src: ["cache.txt"], 
+      		dest: "dist/hello.md"},
+    		]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-rename");
+  grunt.registerTask("default",["rename"]);
+}
