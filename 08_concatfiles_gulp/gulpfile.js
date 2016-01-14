e5833fc7d115f3134f8cfa6a133de799ca347e7d
ics-nohara <nozomi.nohara@ics-web.jp>
2016-01-14 10:55:20 +0900

    gulp - ファイルの結合

---
+++ 08_concatfiles_gulp/gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+ 
+gulp.task("concat", function() {
+  return gulp.src([
+          "css/abc.css",
+          "css/def.css",
+          "css/ghi.js"])
+    .pipe(concat("style.css"))
+    .pipe(gulp.dest("./dist/css/"));
+});
+
+gulp.task("default", ["concat"]);
